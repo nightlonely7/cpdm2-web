9771539174a19b8738c816440a65e3a6d30e8585
NhanNHB <NhanNHB92@gmail.com>
2019-06-06 18:32:57 +0700

    fix notification for DocumentForm

--- src/components/documents/DocumentForm.vue
+++ src/components/documents/DocumentForm.vue
@@ -355,8 +355,8 @@
                         var document = response.data;
                         Axios.get('http://localhost:8080/users/search/findAllDirector').
                         then(response => {
+                            var users = response.data;
                             if (users.length > 0){
-                                var users = response.data;
                                 var title = this.creating ? "Văn bản mới" : "Văn  được chỉnh sửa";
                                 var detail = document.title;
                                 var url = `/documents/${document.id}`;
